0f8db8c27b8fa077f1c7b79142c09973e8d3cd64
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-23 11:59:37 -0600

    add batch option

--- utils/tracking.py
+++ utils/tracking.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 import trackpy as tp
-import imageio
+import numba
 import pims
 
 ########################################################################
@@ -39,13 +39,46 @@ def track(video, output):
     # store data in an HDF5 file
 
     with tp.PandasHDFStore(output) as s:
-        for i in range(0, len(video)):
+        for i in range(0, len(worm_vid)):
             frame = rgb2gray(worm_vid[i][500:-100, 250:])
             sub = frame - max
             features = tp.locate(sub, 35, invert=True,
                                  maxsize=9, minmass=1000)
             s.put(features)
             print(i)
+
+
+def subtract_background(video):
+
+    # read video
+    worm_vid = pims.PyAVVideoReader(video)
+
+    # np array slicing to remove pixels from top, bottom, and left side
+    test_frame = rgb2gray(worm_vid[0][500:-100, 250:])
+
+    # grab the first 25 frames and get the maximum projection
+    first_bit = np.zeros((25, test_frame.shape[0], test_frame.shape[1]))
+    for i in range(0, 25):
+        frame = rgb2gray(worm_vid[i][500:-100, 250:])
+        first_bit[i] = frame
+    max = np.amax(first_bit, axis=0)
+
+    arr = np.zeros((len(worm_vid), test_frame.shape[0], test_frame.shape[1]))
+    for i in range(0, len(worm_vid)):
+        frame = rgb2gray(worm_vid[i][500:-100, 250:])
+        sub = frame - max
+        arr[i] = sub
+        print(i)
+    return arr
+
+
+def track_batch(video, output):
+
+    arr = subtract_background(video)
+
+    with tp.PandasHDFStoreBig(output) as s:
+        tp.batch(arr, 35, invert=True,
+                 maxsize=9, minmass=1000, output=s)
 
 
 if __name__ == '__main__':
@@ -57,8 +90,13 @@ if __name__ == '__main__':
                         help='Path to the video.')
     parser.add_argument('output', type=str,
                         help='Path to the output.')
-
+    parser.add_argument('--batch', action='store_true', default=False,
+                        help='Run in batch mode.')
     args = parser.parse_args()
 
-    track(args.video,
-          args.output)
+    if args.batch:
+        track_batch(args.video,
+                    args.output)
+    else:
+        track(args.video,
+              args.output)
