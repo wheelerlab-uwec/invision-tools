47d5106f36d9b4583dc603a5c0481267ff2c634a
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-24 17:01:17 -0600

    update

--- utils/tracking.py
+++ utils/tracking.py
@@ -21,83 +21,75 @@ from skimage.exposure import rescale_intensity
 
 
 # convert RGB image to greyscale uint8
+@pims.pipeline
 def rgb2gray(rgb):
     gray = np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
     return gray.astype(np.uint8)
 
 
-def get_background(video, output, camera):
-
-    # output base
-    base = Path(output).stem
-
-    # read video
-    worm_vid = pims.PyAVVideoReader(video)
-
-    # np array slicing to remove pixels from top, bottom, and left side
+@pims.pipeline
+def crop(video, camera):
     if camera == 'left':
-        test_frame = rgb2gray(worm_vid[0][500:-100, 250:])
+        cropped = pims.process.crop(video, ((300, 100), (250, 0)))
     else:
-        test_frame = rgb2gray(worm_vid[0][300:-300, :])
+        cropped = pims.process.crop(video, ((200, 200), (0, 0)))
+    return cropped
 
-    # grab the first 25 frames and get the maximum projection
+
+def get_background(video, output):
+
+    base = Path(output).stem
+    first_frame = video[0]
+
     first_bit = np.zeros(
-        (25, test_frame.shape[0], test_frame.shape[1]), np.uint8)
+        (25, first_frame.shape[0], first_frame.shape[1]), np.uint8)
     for i in range(0, 25):
-        if camera == 'left':
-            frame = rgb2gray(worm_vid[i][500:-100, 250:])
-        else:
-            frame = rgb2gray(worm_vid[i][300:-300, :])
-        first_bit[i] = frame
+        first_bit[i] = rgb2gray(video[i])
     max = np.amax(first_bit, axis=0)
     im = Image.fromarray(max)
     im = im.convert("L")
-    save_path = Path(base, f"{base}_background.png")
+    save_path = Path(output, f"{base}_background.png")
     im.save(save_path)
 
-    return max, test_frame
+    return max
 
 
-def process_image(video, output, camera):
+def process_frame(frame, camera, background):
+    gray = rgb2gray(frame)
+    cropped = crop(gray, camera)
+    sub = (cropped - background).astype(np.int8)
+    rescale = rescale_intensity(sub, out_range=(0, 255))
+    inv = (255 - rescale).astype(np.uint8)
+    smooth = gaussian(inv, sigma=5, preserve_range=True)
 
-    # output base
-    base = Path(output).stem
-    os.mkdir(base)
-
-    # read video
-    worm_vid = pims.PyAVVideoReader(video)
-
-    max, test_frame = get_background(video, output, camera)
-
-    arr = np.zeros(
-        (len(worm_vid), test_frame.shape[0], test_frame.shape[1]), np.uint8)
-    for i in range(0, len(worm_vid)):
-        print(f"Processing frame {i}.")
-        if camera == 'left':
-            frame = rgb2gray(worm_vid[i][500:-100, 250:])
-        else:
-            frame = rgb2gray(worm_vid[i][300:-300, :])
-        sub = (frame - max).astype(np.int8)
-        rescale = rescale_intensity(sub, out_range=(0, 255))
-        inv = (255 - rescale).astype(np.uint8)
-        smooth = gaussian(inv, sigma=5, preserve_range=True)
-        arr[i] = smooth
-        if i % 900 == 0:
-            im = Image.fromarray(smooth)
-            im = im.convert("L")
-            save_path = Path(base, f"{base}_{i}.png")
-            im.save(save_path)
-
-    return arr
+    return smooth
 
 
 def track_batch(video, output, camera):
+    base = Path(output).stem
+    os.mkdir(output)
+    worm_vid = pims.PyAVVideoReader(video)
+    worm_vid = worm_vid[0:50]
 
-    arr = process_image(video, output, camera)
+    background = get_background(worm_vid, output)
+    background = crop(background, camera)
 
-    with tp.PandasHDFStoreBig(output) as s:
-        tp.batch(arr, 35, invert=True,
-                 maxsize=9, minmass=1000, topn=50,
+    vid_arr = np.zeros(
+        (len(worm_vid), background.shape[0], background.shape[1]))
+    i = 0
+    for frame in worm_vid:
+        print(f'Processing frame {i}.')
+        arr = process_frame(frame, camera, background)
+        vid_arr[i] = arr
+        if i % 1 == 0:
+            im = Image.fromarray(arr)
+            im = im.convert("L")
+            save_path = Path(output, f"{base}_{i}.png")
+            im.save(save_path)
+        i += 1
+
+    with tp.PandasHDFStoreBig(Path(output, f"{base}.hd5")) as s:
+        tp.batch(vid_arr, 19, minmass=1000,
                  output=s)
 
 
@@ -109,7 +101,7 @@ if __name__ == '__main__':
     parser.add_argument('video', type=str,
                         help='Path to the video.')
     parser.add_argument('output', type=str,
-                        help='Path to the output.')
+                        help='Path to the output directory.')
     parser.add_argument('camera', type=str,
                         help='"left" or "right" camera.')
     args = parser.parse_args()
