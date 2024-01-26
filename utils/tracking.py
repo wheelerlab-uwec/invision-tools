f09091cc5f1e2d0d2e22d2566160e5cbce25f672
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-26 15:16:51 -0600

    adjust logic

--- utils/tracking.py
+++ utils/tracking.py
@@ -25,28 +25,9 @@ def rgb2gray(frame):
     return gray
 
 
-def get_background(video, output, i):
-
-    first_frame = video[i]
-
-    first_bit = np.zeros(
-        (50, first_frame.shape[0], first_frame.shape[1]), np.uint8)
-    j = 0
-    for k in range(i, i + 50):
-        frame = video[k].asnumpy()
-        first_bit[j] = rgb2gray(frame)
-        j += 1
-        del frame
-    max = np.amax(first_bit, axis=0)
-    save_path = Path(output, f"background_{i}.png")
-    cv2.imwrite(str(save_path), max)
-
-    return max
-
-
 def process_frame(frame, background):
-    gray = rgb2gray(frame)
-    sub = (gray - background).astype(np.int8)
+    # gray = rgb2gray(frame)
+    sub = (frame - background).astype(np.int8)
     rescale = rescale_intensity(sub,
                                 out_range='dtype')
     inv = (255 - rescale).astype(np.uint8)
@@ -55,29 +36,46 @@ def process_frame(frame, background):
     return smooth
 
 
+def process_chunk(chunk, output):
+
+    background = np.amax(chunk, axis=0)
+
+    for i in range(chunk.shape[0]):
+        print(f'Processing {i}th frame of chunk.')
+        frame = chunk[i]
+        arr = process_frame(frame, background)
+        chunk[i] = arr
+        if i == 25:
+            save_path = Path(output, f'{i}.png')
+            cv2.imwrite(str(save_path), arr)
+
+    return chunk
+
+
 def track_batch(video, output):
     base = Path(output).stem
     os.mkdir(output)
     worm_vid = decord.VideoReader(video, ctx=decord.cpu(0))
+    shape = worm_vid[0].shape
 
-    # get an initial background
-    background = get_background(worm_vid, output, 0)
+    # initialize array of entire video
+    vid_arr = np.empty((1, shape[0], shape[1]), np.uint8)
 
-    vid_arr = np.zeros(
-        (len(worm_vid), background.shape[0], background.shape[1]), np.uint8)
-    i = 0
-    for i in range(len(worm_vid)):
-        print(f'Processing frame {i}.')
-        frame = worm_vid[i].asnumpy()
-        # refresh the background every 50 frames
-        if i % 50 == 0:
-            background = get_background(worm_vid, output, i)
-        arr = process_frame(frame, background)
-        vid_arr[i] = arr
-        if i % 225 == 0:
-            save_path = Path(output, f"{base}_{i}.png")
-            cv2.imwrite(str(save_path), arr)
-        i += 1
+    # load 50 frames into memory
+    chunk_size = 50
+    chunk = np.zeros((chunk_size, shape[0], shape[1]), np.uint8)
+    for c in range(0, int(len(worm_vid) / chunk_size)):
+        print(
+            f"Processing frames {c * chunk_size + 1} - {c * chunk_size + chunk_size}")
+        frame_numbers = range(c * chunk_size + 1, c * chunk_size + chunk_size)
+        i = 0
+        for frame_number in frame_numbers:
+            frame = rgb2gray(worm_vid[frame_number].asnumpy())
+            chunk[i] = frame
+            i += 1
+        processed = process_chunk(chunk, output)
+        vid_arr = np.vstack((vid_arr, processed))
+        print(vid_arr.shape)
 
     with tp.PandasHDFStoreBig(Path(output, f"{base}.hd5")) as s:
         tp.batch(vid_arr, 19, minmass=1000,
