98714d2061fdb4639a30844a26f8e340a67c5e57
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-01-26 22:05:25 -0600

    try again

--- utils/tracking.py
+++ utils/tracking.py
@@ -5,13 +5,11 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 import trackpy as tp
-from PIL import Image
 from pathlib import Path
 import os
 from skimage.filters import gaussian
 from skimage.exposure import rescale_intensity
 import cv2
-import decord
 
 ########################################################################
 ####                                                                ####
@@ -20,66 +18,63 @@ import decord
 ########################################################################
 
 
+# convert RGB image to greyscale uint8
 def rgb2gray(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY).astype(np.uint8)
-    return gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    return gray.astype(np.uint8)
 
 
 def process_frame(frame, background):
     # gray = rgb2gray(frame)
     sub = (frame - background).astype(np.int8)
-    rescale = rescale_intensity(sub,
-                                out_range='dtype')
+    rescale = rescale_intensity(sub, out_range=(0, 255))
     inv = (255 - rescale).astype(np.uint8)
     smooth = gaussian(inv, sigma=5, preserve_range=True)
 
     return smooth
 
 
-def process_chunk(chunk, output):
-
-    background = np.amax(chunk, axis=0)
-
-    for i in range(chunk.shape[0]):
-        print(f'Processing {i}th frame of chunk.')
-        frame = chunk[i]
-        arr = process_frame(frame, background)
-        chunk[i] = arr
-        if i == 50:
-            save_path = Path(output, f'{i}.png')
-            cv2.imwrite(str(save_path), arr)
-
-    return chunk
-
-
 def track_batch(video, output):
     base = Path(output).stem
     os.mkdir(output)
-    worm_vid = decord.VideoReader(video, ctx=decord.cpu(0))
-    shape = worm_vid[0].shape
+    worm_vid = cv2.VideoCapture(video)
+    num_frames = int(worm_vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # initialize array of entire video
-    vid_arr = np.empty((1, shape[0], shape[1]), np.uint8)
+    ret, frame = worm_vid.read()
+    if ret:
+        frame_shape = frame.shape
 
-    # load 50 frames into memory
-    chunk_size = 50
-    chunk = np.zeros((chunk_size, shape[0], shape[1]), np.uint8)
-    for c in range(0, int(len(worm_vid) / chunk_size)):
-        print(
-            f"Processing frames {c * chunk_size + 1} - {c * chunk_size + chunk_size}")
-        frame_numbers = range(c * chunk_size + 1, c * chunk_size + chunk_size)
-        i = 0
-        for frame_number in frame_numbers:
-            frame = rgb2gray(worm_vid[frame_number].asnumpy())
-            chunk[i] = frame
-            i += 1
-        processed = process_chunk(chunk, output)
-        vid_arr = np.vstack((vid_arr, processed))
-        print(vid_arr.shape)
+    # Reset the video capture to the first frame
+    worm_vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    worm_arr = np.zeros(
+        (num_frames, frame_shape[0], frame_shape[1]), np.uint8)
+    for i in range(num_frames):
+
+        print(f'Loading frame {i} to memory.')
+        ret, frame = worm_vid.read()
+        if not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        worm_arr[i] = frame
+
+    print('Calculating background.')
+    background = np.amax(worm_arr, axis=0)
+
+    i = 0
+    for frame in worm_arr:
+        print(f'Processing frame {i}')
+        arr = process_frame(frame, background)
+        worm_arr[i] = arr
+        if i % 5 == 0:
+            save_path = Path(output, f"{base}_{i}.png")
+            cv2.imwrite(str(save_path), arr)
+        i += 1
 
     with tp.PandasHDFStoreBig(Path(output, f"{base}.hd5")) as s:
-        tp.batch(vid_arr, 19, minmass=1000,
-                 output=s)
+        for image in worm_arr:
+            tp.batch(worm_arr, 19, minmass=1000,
+                     output=s)
 
 
 if __name__ == '__main__':
@@ -91,8 +86,11 @@ if __name__ == '__main__':
                         help='Path to the video.')
     parser.add_argument('output', type=str,
                         help='Path to the output directory.')
+    # parser.add_argument('camera', type=str,
+    #                     help='"left" or "right" camera.')
     args = parser.parse_args()
 
     track_batch(args.video,
                 args.output
+                # args.camera
                 )
