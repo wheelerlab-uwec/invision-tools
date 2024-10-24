77fd7c24bf2ca38abdde063773fb59793f09b447
wheelern <wheelenj@uwec.edu>
2024-10-24 14:04:28 -0500

    separate tracking/linking, for now

--- slurm/track_highmemory.sh
+++ slurm/track_highmemory.sh
@@ -29,5 +29,5 @@ export PYTHONUNBUFFERED=TRUE
 python ~/GitHub/invision-tools/utils/tracking.py $PWD/00000${SLURM_ARRAY_TASK_ID}.mp4 $PWD/00000${SLURM_ARRAY_TASK_ID}
 mv $PWD/00000${SLURM_ARRAY_TASK_ID}/00000${SLURM_ARRAY_TASK_ID}.hdf5 $PWD
 
-echo "Linking trajectories"
-python ~/GitHub/invision-tools/utils/link_trajectories.py $PWD --hdf5 
+# echo "Linking trajectories"
+# python ~/GitHub/invision-tools/utils/link_trajectories.py $PWD --hdf5 
