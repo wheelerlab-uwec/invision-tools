28f47f38e70725ca6f59c8a25735e00f282c6755
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2024-11-19 11:05:12 -0600

    template

--- testing/link_merge.py
+++ testing/link_merge.py
@@ -5,10 +5,10 @@ import gzip
 
 
 right = pd.read_pickle(
-    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240718/20240718-a01-RVH_20240718_142342.24568709/20240718-a01-RVH_20240718_142342_tracks.pkl.gz"
+    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240725/20240725-a04-RVH_20240725_142210.24568709/20240725-a04-RVH_20240725_142210_tracks.pkl.gz"
 )
 left = pd.read_pickle(
-    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240718/20240718-a01-RVH_20240718_142342.24568744/20240718-a01-RVH_20240718_142342_tracks.pkl.gz"
+    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240725/20240725-a04-RVH_20240725_142210.24568744/20240725-a04-RVH_20240725_142210_tracks.pkl.gz"
 )
 
 # Fix for TypeError: 'float' object is not callable
@@ -31,7 +31,7 @@ t = tp.link(
     combined, search_range=search_range, memory=memory, adaptive_stop=adaptive_stop
 )
 
-pickle_path = "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240718/20240718-a01-RVH_tracks.pkl.gz"
+pickle_path = "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240725/20240725-a04-RVH_tracks.pkl.gz"
 
 with gzip.open(pickle_path, "wb") as f:
     print("Writing pickle file.")
@@ -102,7 +102,43 @@ t = tp.link(
     combined, search_range=search_range, memory=memory, adaptive_stop=adaptive_stop
 )
 
-pickle_path = "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240619/20240619-a01-RVH_tracks.pkl.gz"
+pickle_path = "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240829/20240829-a01-RVH.pkl.gz"
+
+with gzip.open(pickle_path, "wb") as f:
+    print("Writing pickle file.")
+    pickle.dump(t, f)
+
+
+###########################################################
+
+right = pd.read_pickle(
+    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240619/20240619-a01-RVH_20240619_140433.24568709/20240619-a01-RVH_20240619_140433_tracks.pkl.gz"
+)
+left = pd.read_pickle(
+    "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240619/20240619-a01-RVH_20240619_140433.24568744/20240619-a01-RVH_20240619_140433_tracks.pkl.gz"
+)
+
+# Fix for TypeError: 'float' object is not callable
+max_x_val = max(right["x"])
+min_x_val = min(right["x"])
+right["x"] = max_x_val - right["x"] + min_x_val - 475
+right = right[right["x"] >= -10]
+right["y"] = right["y"] - 125
+right = right.drop("particle", axis=1)
+
+left = left.drop("particle", axis=1)
+left["x"] = left["x"] * -1
+combined = pd.concat([left, right])
+
+search_range = 45
+memory = 25
+adaptive_stop = 30
+
+t = tp.link(
+    combined, search_range=search_range, memory=memory, adaptive_stop=adaptive_stop
+)
+
+pickle_path = "/Users/njwheeler/Library/CloudStorage/OneDrive-UW-EauClaire/WheelerLab/Data/project-miracidia_sensation/invision/double_agar/20240829/20240829-a01-RVH.pkl.gz"
 
 with gzip.open(pickle_path, "wb") as f:
     print("Writing pickle file.")
